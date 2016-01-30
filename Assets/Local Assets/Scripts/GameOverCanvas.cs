700a32abbbe941ebac78cd4ee28d8e96b4e01074
Miguel Leonardo Correa Angarita <mlca@MacBook-Pro-de-Miguel.local>
2016-01-30 15:40:37 -0500

    Sistema de trampas, menu y escena gameover

---
+++ Assets/Local Assets/Scripts/GameOverCanvas.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOverCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Restart()
+	{
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
