c88d14ab512d55a43972bf5020adb30b57688f19
Miguel Leonardo Correa Angarita <mlca@MacBook-Pro-de-Miguel.local>
2016-01-30 01:33:24 -0500

    MainScene

--- Assets/Local Assets/Scripts/Camera.cs
+++ Assets/Local Assets/Scripts/Camera.cs
@@ -4,14 +4,33 @@ using System.Collections;
 public class Camera : MonoBehaviour {
 
 	public GameObject Player;
+	private float minXCamera;
+	private float minYCamera;
+	private float maxXCamera;
+	private float maxYCamera;
+	private float positionX;
+	private float positionY;
 	// Use this for initialization
 	void Start () {
-	
+		minXCamera = -35.0f;
+		minYCamera = 0.0f;
+		maxXCamera = 35.1f;
+		maxYCamera = 39.6f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+		if (Player.transform.position.x > minXCamera && Player.transform.position.x < maxXCamera) {
+			positionX = Player.transform.position.x;
+		} else {
+			positionX = transform.position.x;
+		}
+		if (Player.transform.position.y > minYCamera && Player.transform.position.y < maxYCamera) {
+			positionY = Player.transform.position.y;
+		} else {
+			positionY = transform.position.y;
+		}
+		transform.position = new Vector3(positionX, positionY, -10);
 		//transform.LookAt (Player.transform.position);
 	}
 }
