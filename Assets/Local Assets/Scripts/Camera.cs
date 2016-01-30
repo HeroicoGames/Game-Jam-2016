bba6cc015a31daaae4abd6e40c71ec502a8e8c5a
Miguel Leonardo Correa Angarita <mlca@MacBook-Pro-de-Miguel.local>
2016-01-29 23:10:29 -0500

    Enemy prefab, Wall prefab, Camera script

---
+++ Assets/Local Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10);
+		//transform.LookAt (Player.transform.position);
+	}
+}
---
+++ Assets/Local Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float speed;
+	private Vector2 playerPosition;
+	// Use this for initialization
+	void Start () {
+		speed = 2.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Move (0, 1);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			Move (0,-1);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			Move (-1,0);
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			Move (1,0);
+		}
+	
+	}
+
+	public void Move(float stepX, float stepY)
+	{
+		Vector3 newPosition = new Vector3 (transform.position.x+stepX*speed*Time.deltaTime,transform.position.y+stepY*speed*Time.deltaTime, 0f);
+		transform.position = newPosition;
+		stepX = 0;
+		stepY = 0;
+	}
+}
