9ea0748179886d3921a1ca329f7e9fef3ed3029b
Alonso <alons45@aol.com>
2016-01-30 01:29:08 -0500

    Reduced blur gradually

--- Assets/Local Assets/Scripts/BlurManager.cs
+++ Assets/Local Assets/Scripts/BlurManager.cs
@@ -7,18 +7,37 @@ public class BlurManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine ("removeBlur");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		removeBlur ();
+
 	}
 
-	void removeBlur() {
-		
+	IEnumerator removeBlur() {
 		BlurOptimized b = gameObject.GetComponent<BlurOptimized> ();
+
+		for (int i = 20; i >= 0; i -= 1) {
+
+			// b.downsample -= 1;
+			b.blurSize -= 0.5f;
+			// b.blurIterations -= 1;
+
+			yield return new WaitForSeconds(0.5f);
+		}
+
+		yield return new WaitForSeconds (0.5f);
 		b.downsample = 0;
-		b.blurSize = 0;
+
+		yield return new WaitForSeconds (0.5f);
+		b.blurIterations = 2;
+
+		yield return new WaitForSeconds (0.5f);
+		b.blurIterations = 1;
+
+		yield return new WaitForSeconds (0.5f);
+		b.enabled = false;
+			
 	}
 }
