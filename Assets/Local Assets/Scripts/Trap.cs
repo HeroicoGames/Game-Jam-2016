379a56f2230955b33d286eaf6b31f61490c33a57
Miguel Leonardo Correa Angarita <mlca@MacBook-Pro-de-Miguel.local>
2016-01-30 11:28:25 -0500

    Cambios en el escenario

---
+++ Assets/Local Assets/Scripts/Trap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		sprite = transform.gameObject.GetComponent<SpriteRenderer> ();
+		sprite.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
