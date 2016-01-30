71241854c1b40d9e5bacfd75104fcfd9c1217734
Alonso <alons45@aol.com>
2016-01-30 00:23:02 -0500

    Add new assets and blur effect

---
+++ Assets/Local Assets/Scripts/BlurManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+
+public class BlurManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		removeBlur ();
+	}
+
+	void removeBlur() {
+		
+		BlurOptimized b = gameObject.GetComponent<BlurOptimized> ();
+		b.downsample = 0;
+		b.blurSize = 0;
+	}
+}
